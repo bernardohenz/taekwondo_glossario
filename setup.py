0d276211861d9675df24cfbf04ff962f54288ac8
Bernardo Henz <bhenz@nicolab.com>
2025-05-08 21:50:55 -0300

    Small fix

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="taekwondo",
+    name="taekwondo_glossario",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "streamlit>=1.32.0",
+        "python-Levenshtein==0.23.0",
     ],
 )
